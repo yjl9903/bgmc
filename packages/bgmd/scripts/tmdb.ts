4898f6e5ea1599527bbb51b2fbcb9d24899f6cd9
XLor <yjl9903@vip.qq.com>
2023-10-12 17:02:58 +0800

    feat(bgmd): extract tmdb search

--- packages/bgmd/scripts/tmdb.ts
+++ packages/bgmd/scripts/tmdb.ts
@@ -7,9 +7,9 @@ import { items, type Item } from 'bangumi-data';
 import {
   TMDBClient,
   type SearchResultItem,
-  SearchTVResultItem,
-  SearchMovieResultItem,
-  SearchMultiResultItem
+  type SearchTVResultItem,
+  type SearchMovieResultItem,
+  type SearchMultiResultItem
 } from 'tmdbc';
 
 import { ufetch } from './ufetch';
@@ -19,6 +19,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const dataRoot = path.join(__dirname, '../../../data/tmdb');
 await fs.ensureDir(dataRoot);
+
+const client = new TMDBClient({
+  baseURL: 'https://movies-proxy.vercel.app/tmdb/',
+  fetch: ufetch,
+  token: ''
+});
 
 async function main() {
   const files = groupByBegin(items);
@@ -43,13 +49,8 @@ interface TMDBItem {
 }
 
 async function downloadSubject(file: string, items: Item[]) {
-  const client = new TMDBClient({
-    baseURL: 'https://movies-proxy.vercel.app/tmdb/',
-    fetch: ufetch,
-    token: ''
-  });
-
   const bangumis: TMDBItem[] = [];
+
   for (const item of items) {
     const bgm = item.sites.find((site) => site.site === 'bangumi');
     if (!bgm) continue;
@@ -62,85 +63,87 @@ async function downloadSubject(file: string, items: Item[]) {
         tmdb: result.ok
       });
     }
-
-    async function search(item: Item) {
-      const all: SearchResultItem[] = [];
-      const names = new Set([item.title, ...Object.values(item.titleTranslate).flat()]);
-      for (const query of names) {
-        const resp =
-          item.type === 'movie'
-            ? await client.searchMovie({ query, language: 'zh-CN' })
-            : item.type === 'tv'
-            ? await client.searchTV({ query, language: 'zh-CN' })
-            : await client.searchMulti({ query, language: 'zh-CN' });
-        if (resp.results.length > 0) {
-          const result = inferBangumi(item, resp.results);
-          all.push(...result.all);
-          if (result.ok) {
-            return result;
-          }
-        }
-      }
-      if (all.length === 0) {
-        console.log(`Error: There is no search result for ${item.title}`);
-      } else {
-        console.log(`Error: There is multiple search results for ${item.title}`);
-      }
-      return { ok: undefined, all };
-    }
-
-    function inferBangumi(
-      item: Item,
-      resp: (SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem)[]
-    ) {
-      const filtered = resp.filter((r) => matchBangumi(item, r));
-      if (resp.length === 1 && filtered.length === 1) {
-        return { ok: resp[0], all: filtered };
-      } else {
-        if (filtered.length === 1) {
-          const result = filtered[0];
-          console.log(
-            `Info: infer ${item.title} to ${getOriginalNameOrTitle(result)} (id: ${result.id})`
-          );
-          return { ok: result, all: filtered };
-        }
-        return { ok: undefined, all: filtered };
-      }
-    }
-
-    function matchBangumi(
-      item: Item,
-      result: SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem
-    ) {
-      const d1 = new Date(item.begin);
-      // @ts-ignore
-      const d2 = new Date(result.first_air_date || result.release_date);
-      // Onair date should be less than 7 days
-      if (Math.abs(d1.getTime() - d2.getTime()) > 7 * 24 * 60 * 60 * 1000) {
-        return false;
-      }
-      return true;
-    }
-
-    function getNameOrTitle(
-      resp: SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem
-    ) {
-      // @ts-ignore
-      return resp.name || resp.title;
-    }
-
-    function getOriginalNameOrTitle(
-      resp: SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem
-    ) {
-      // @ts-ignore
-      return resp.original_name || resp.original_title;
-    }
   }
 
   await fs.writeFile(file, JSON.stringify(bangumis, null, 2));
 }
 
+async function search(item: Item) {
+  const all: SearchResultItem[] = [];
+  const names = new Set([item.title, ...Object.values(item.titleTranslate).flat()]);
+  for (const query of names) {
+    const resp =
+      item.type === 'movie'
+        ? await client.searchMovie({ query, language: 'zh-CN' })
+        : item.type === 'tv'
+        ? await client.searchTV({ query, language: 'zh-CN' })
+        : await client.searchMulti({ query, language: 'zh-CN' });
+    if (resp.results.length > 0) {
+      const result = inferBangumi(item, resp.results);
+      all.push(...result.all);
+      if (result.ok) {
+        return result;
+      }
+    }
+  }
+  if (all.length === 0) {
+    console.log(`Error: There is no search result for ${item.title}`);
+  } else {
+    console.log(`Error: There is multiple search results for ${item.title}`);
+  }
+  return { ok: undefined, all };
+
+  function inferBangumi(
+    item: Item,
+    resp: (SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem)[]
+  ) {
+    const filtered = resp.filter((r) => matchBangumi(item, r));
+    if (resp.length === 1 && filtered.length === 1) {
+      return { ok: resp[0], all: filtered };
+    } else {
+      if (filtered.length === 1) {
+        const result = filtered[0];
+        console.log(
+          `Info: infer ${item.title} to ${getOriginalNameOrTitle(result)} (id: ${result.id})`
+        );
+        return { ok: result, all: filtered };
+      }
+      return { ok: undefined, all: filtered };
+    }
+  }
+
+  function matchBangumi(
+    item: Item,
+    result: SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem
+  ) {
+    const d1 = new Date(item.begin);
+    // @ts-ignore
+    const d2 = new Date(result.first_air_date || result.release_date);
+    // Onair date should be less than 7 days
+    if (Math.abs(d1.getTime() - d2.getTime()) > 7 * 24 * 60 * 60 * 1000) {
+      return false;
+    }
+    return true;
+  }
+
+  function getNameOrTitle(
+    resp: SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem
+  ) {
+    // @ts-ignore
+    return resp.name || resp.title;
+  }
+
+  function getOriginalNameOrTitle(
+    resp: SearchTVResultItem | SearchMovieResultItem | SearchMultiResultItem
+  ) {
+    // @ts-ignore
+    return resp.original_name || resp.original_title;
+  }
+}
+
 const cli = breadc('tmdb');
+
+cli.command('search <keyword>').action((keyword) => {});
 
 cli.command('').action(async (options) => {
   await main();
